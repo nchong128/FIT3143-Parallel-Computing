af72415079e856d0e4b0f305271936b791dbe842
Nicholas Lin Ren Chong <ncho0010@student.monash.edu>
2020-09-04 13:27:03 +1200

    Backing up week 5

---
+++ Week 5/Lab/task3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+struct valuestruct {
+    int a;
+    double b;
+};
+
+int main(int argc, char* argv[]) {
+    int my_rank;
+    int p;
+    struct valuestruct values;
+    MPI_Datatype Valuetype;
+    MPI_Datatype type[2] = { MPI_INT, MPI_DOUBLE };
+    int blocklen[2] = { 1, 1 };
+    MPI_Aint disp[2];
+
+    
+    MPI_Init( &argc, &argv );
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    MPI_Get_address(&values.a, &disp[0]);
+    MPI_Get_address(&values.b, &disp[1]);
+
+    disp[1] = disp[1]-disp[0];
+    disp[0] = 0;
+
+    MPI_Type_create_struct(2, blocklen, disp, type, &Valuetype);
+    MPI_Type_commit(&Valuetype);
+    
+    do {
+        if (my_rank == 0) {
+            printf("Enter a round number >0 & a real number:");
+            fflush(stdout);
+            scanf("%d%lf", &values.a, &values.b);
+        }
+        MPI_Bcast(&values, 2, Valuetype, 0, MPI_COMM_WORLD);
+        printf("Rank: %d. values.a = %d. values.b = %lf\n", my_rank, values.a, values.b);
+        fflush(stdout);
+    } while (values.a > 0);
+
+    MPI_Type_free(&Valuetype);
+    MPI_Finalize();
+    return 0;
+}
